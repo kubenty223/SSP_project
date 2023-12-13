d90d9e1b0a90ae72924d53427ff1561926fa4b30
Michał Kaszuba <56135959+kaszubam9@users.noreply.github.com>
2023-12-13 20:17:14 +0100

    Update request_file.py

--- request_file.py
+++ request_file.py
@@ -6,25 +6,25 @@ import os
 import time
 
 def pobierz_plik(url, nazwa_lokalna):
-    
-	os.system("wget '{}' -O '{}'".format(url, nazwa_lokalna))
-	print('Plik pobrany i zapisany jako {}'.format(nazwa_lokalna))
+
+        os.system("wget '{}' -O '{}'".format(url, nazwa_lokalna))
+        print('Plik pobrany i zapisany jako {}'.format(nazwa_lokalna))
 
 
 
 
 if __name__ == '__main__':
 
-	while(1):
-		adres = sys.argv[1]
-		liczba = random.randint(1, 3)
-		nazwa_pliku = 'file_{}.txt'.format(str(liczba))
+        while(1):
+                adres = sys.argv[1]
+                host = str(sys.argv[2])
+                liczba = random.randint(1, 3)
+                nazwa_pliku = 'file_{}.txt'.format(str(liczba))
 
 
-		url = 'http://{}/{}'.format(adres, nazwa_pliku)
-	
-		nazwa_lokalna = 'zapis_{}'.format(nazwa_pliku)
+                url = 'http://{}/{}'.format(adres, nazwa_pliku)
 
-		pobierz_plik(url, nazwa_lokalna)
-		time.sleep(random.randint(1, 5))
+                nazwa_lokalna = '{}/zapis_{}'.format(host, nazwa_pliku)
 
+                pobierz_plik(url, nazwa_lokalna)
+                time.sleep(random.randint(1, 5))
